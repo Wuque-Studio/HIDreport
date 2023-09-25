3d1e2a877f4c60d649a51946909dadaba4b6e81a
spbgzh <72414103+spbgzh@users.noreply.github.com>
2023-09-25 08:12:32 +0300

    update

--- main.c
+++ main.c
@@ -6,165 +6,166 @@
  * HID report descriptors
  */
 const uint8_t HIDReport[] = {
-    HID_RI_USAGE_PAGE(8, 0x01),        // Generic Desktop
-    HID_RI_USAGE(8, 0x06),             // Keyboard
-    HID_RI_COLLECTION(8, 0x01),        // Application
+    HID_RI_USAGE_PAGE(8, 0x01),00,        // Generic Desktop
+    
+    HID_RI_USAGE(8, 0x06),00,             // Keyboard
+    HID_RI_COLLECTION(8, 0x01),00,        // Application
 
-        HID_RI_REPORT_ID(8, 1),
+        HID_RI_REPORT_ID(8, 1),00,
 
         // Modifiers (8 bits)
-        HID_RI_USAGE_PAGE(8, 0x07),    // Keyboard/Keypad
-        HID_RI_USAGE_MINIMUM(8, 0xE0), // Keyboard Left Control
-        HID_RI_USAGE_MAXIMUM(8, 0xE7), // Keyboard Right GUI
-        HID_RI_LOGICAL_MINIMUM(8, 0x00),
-        HID_RI_LOGICAL_MAXIMUM(8, 0x01),
-        HID_RI_REPORT_COUNT(8, 0x08),
-        HID_RI_REPORT_SIZE(8, 0x01),
-        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+        HID_RI_USAGE_PAGE(8, 0x07),00,    // Keyboard/Keypad
+        HID_RI_USAGE_MINIMUM(8, 0xE0),00, // Keyboard Left Control
+        HID_RI_USAGE_MAXIMUM(8, 0xE7),00, // Keyboard Right GUI
+        HID_RI_LOGICAL_MINIMUM(8, 0x00),00,
+        HID_RI_LOGICAL_MAXIMUM(8, 0x01),00,
+        HID_RI_REPORT_COUNT(8, 0x08),00,
+        HID_RI_REPORT_SIZE(8, 0x01),00,
+        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),00,
         // Reserved (1 byte)
-        HID_RI_REPORT_COUNT(8, 0x01),
-        HID_RI_REPORT_SIZE(8, 0x08),
-        HID_RI_INPUT(8, HID_IOF_CONSTANT),
+        HID_RI_REPORT_COUNT(8, 0x01),00,
+        HID_RI_REPORT_SIZE(8, 0x08),00,
+        HID_RI_INPUT(8, HID_IOF_CONSTANT),00,
         // Keycodes (6 bytes)
-        HID_RI_USAGE_PAGE(8, 0x07),    // Keyboard/Keypad
-        HID_RI_USAGE_MINIMUM(8, 0x00),
-        HID_RI_USAGE_MAXIMUM(8, 0xFF),
-        HID_RI_LOGICAL_MINIMUM(8, 0x00),
-        HID_RI_LOGICAL_MAXIMUM(16, 0x00FF),
-        HID_RI_REPORT_COUNT(8, 0x06),
-        HID_RI_REPORT_SIZE(8, 0x08),
-        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),
+        HID_RI_USAGE_PAGE(8, 0x07),00,    // Keyboard/Keypad
+        HID_RI_USAGE_MINIMUM(8, 0x00),00,
+        HID_RI_USAGE_MAXIMUM(8, 0xFF),00,
+        HID_RI_LOGICAL_MINIMUM(8, 0x00),00,
+        HID_RI_LOGICAL_MAXIMUM(16, 0x00FF),00,
+        HID_RI_REPORT_COUNT(8, 0x06),00,
+        HID_RI_REPORT_SIZE(8, 0x08),00,
+        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),00,
 
         // Status LEDs (5 bits)
-        HID_RI_USAGE_PAGE(8, 0x08),    // LED
-        HID_RI_USAGE_MINIMUM(8, 0x01), // Num Lock
-        HID_RI_USAGE_MAXIMUM(8, 0x05), // Kana
-        HID_RI_LOGICAL_MINIMUM(8, 0x00),
-        HID_RI_LOGICAL_MAXIMUM(8, 0x01),
-        HID_RI_REPORT_COUNT(8, 0x05),
-        HID_RI_REPORT_SIZE(8, 0x01),
-        HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE | HID_IOF_NON_VOLATILE),
+        HID_RI_USAGE_PAGE(8, 0x08),00,    // LED
+        HID_RI_USAGE_MINIMUM(8, 0x01),00, // Num Lock
+        HID_RI_USAGE_MAXIMUM(8, 0x05),00, // Kana
+        HID_RI_LOGICAL_MINIMUM(8, 0x00),00,
+        HID_RI_LOGICAL_MAXIMUM(8, 0x01),00,
+        HID_RI_REPORT_COUNT(8, 0x05),00,
+        HID_RI_REPORT_SIZE(8, 0x01),00,
+        HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE | HID_IOF_NON_VOLATILE),00,
         // LED padding (3 bits)
-        HID_RI_REPORT_COUNT(8, 0x01),
-        HID_RI_REPORT_SIZE(8, 0x03),
-        HID_RI_OUTPUT(8, HID_IOF_CONSTANT),
-    HID_RI_END_COLLECTION(0),
+        HID_RI_REPORT_COUNT(8, 0x01),00,
+        HID_RI_REPORT_SIZE(8, 0x03),00,
+        HID_RI_OUTPUT(8, HID_IOF_CONSTANT),00,
+    HID_RI_END_COLLECTION(0),00,
 
-    HID_RI_USAGE_PAGE(8, 0x01),            // Generic Desktop
-    HID_RI_USAGE(8, 0x02),                 // Mouse
-    HID_RI_COLLECTION(8, 0x01),            // Application
+    HID_RI_USAGE_PAGE(8, 0x01),00,            // Generic Desktop
+    HID_RI_USAGE(8, 0x02),00,                 // Mouse
+    HID_RI_COLLECTION(8, 0x01),00,            // Application
 
-        HID_RI_REPORT_ID(8, 2),
-        HID_RI_USAGE(8, 0x01),             // Pointer
-        HID_RI_COLLECTION(8, 0x00),        // Physical
+        HID_RI_REPORT_ID(8, 2),00,
+        HID_RI_USAGE(8, 0x01),00,             // Pointer
+        HID_RI_COLLECTION(8, 0x00),00,        // Physical
             // Buttons (8 bits)
-            HID_RI_USAGE_PAGE(8, 0x09),    // Button
-            HID_RI_USAGE_MINIMUM(8, 0x01), // Button 1
-            HID_RI_USAGE_MAXIMUM(8, 0x08), // Button 8
-            HID_RI_LOGICAL_MINIMUM(8, 0x00),
-            HID_RI_LOGICAL_MAXIMUM(8, 0x01),
-            HID_RI_REPORT_COUNT(8, 0x08),
-            HID_RI_REPORT_SIZE(8, 0x01),
-            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+            HID_RI_USAGE_PAGE(8, 0x09),00,    // Button
+            HID_RI_USAGE_MINIMUM(8, 0x01),00, // Button 1
+            HID_RI_USAGE_MAXIMUM(8, 0x08),00, // Button 8
+            HID_RI_LOGICAL_MINIMUM(8, 0x00),00,
+            HID_RI_LOGICAL_MAXIMUM(8, 0x01),00,
+            HID_RI_REPORT_COUNT(8, 0x08),00,
+            HID_RI_REPORT_SIZE(8, 0x01),00,
+            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),00,
 
 
             // Boot protocol XY ignored in Report protocol
-            HID_RI_REPORT_COUNT(8, 0x02),
-            HID_RI_REPORT_SIZE(8, 0x08),
-            HID_RI_INPUT(8, HID_IOF_CONSTANT),
+            HID_RI_REPORT_COUNT(8, 0x02),00,
+            HID_RI_REPORT_SIZE(8, 0x08),00,
+            HID_RI_INPUT(8, HID_IOF_CONSTANT),00,
 
             // X/Y position (2 or 4 bytes)
-            HID_RI_USAGE_PAGE(8, 0x01),    // Generic Desktop
-            HID_RI_USAGE(8, 0x30),         // X
-            HID_RI_USAGE(8, 0x31),         // Y
+            HID_RI_USAGE_PAGE(8, 0x01),00,    // Generic Desktop
+            HID_RI_USAGE(8, 0x30),00,         // X
+            HID_RI_USAGE(8, 0x31),00,         // Y
 
-            HID_RI_LOGICAL_MINIMUM(16, -32767),
-            HID_RI_LOGICAL_MAXIMUM(16,  32767),
-            HID_RI_REPORT_COUNT(8, 0x02),
-            HID_RI_REPORT_SIZE(8, 0x10),
-            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_RELATIVE),
+            HID_RI_LOGICAL_MINIMUM(16, -32767),00,
+            HID_RI_LOGICAL_MAXIMUM(16,  32767),00,
+            HID_RI_REPORT_COUNT(8, 0x02),00,
+            HID_RI_REPORT_SIZE(8, 0x10),00,
+            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_RELATIVE),00,
 
             // Vertical wheel (1 byte)
-            HID_RI_USAGE(8, 0x38),         // Wheel
-            HID_RI_LOGICAL_MINIMUM(8, -127),
-            HID_RI_LOGICAL_MAXIMUM(8, 127),
-            HID_RI_REPORT_COUNT(8, 0x01),
-            HID_RI_REPORT_SIZE(8, 0x08),
-            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_RELATIVE),
+            HID_RI_USAGE(8, 0x38),00,         // Wheel
+            HID_RI_LOGICAL_MINIMUM(8, -127),00,
+            HID_RI_LOGICAL_MAXIMUM(8, 127),00,
+            HID_RI_REPORT_COUNT(8, 0x01),00,
+            HID_RI_REPORT_SIZE(8, 0x08),00,
+            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_RELATIVE),00,
             // Horizontal wheel (1 byte)
-            HID_RI_USAGE_PAGE(8, 0x0C),    // Consumer
-            HID_RI_USAGE(16, 0x0238),      // AC Pan
-            HID_RI_LOGICAL_MINIMUM(8, -127),
-            HID_RI_LOGICAL_MAXIMUM(8, 127),
-            HID_RI_REPORT_COUNT(8, 0x01),
-            HID_RI_REPORT_SIZE(8, 0x08),
-            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_RELATIVE),
-        HID_RI_END_COLLECTION(0),
-    HID_RI_END_COLLECTION(0),
+            HID_RI_USAGE_PAGE(8, 0x0C),00,    // Consumer
+            HID_RI_USAGE(16, 0x0238),00,      // AC Pan
+            HID_RI_LOGICAL_MINIMUM(8, -127),00,
+            HID_RI_LOGICAL_MAXIMUM(8, 127),00,
+            HID_RI_REPORT_COUNT(8, 0x01),00,
+            HID_RI_REPORT_SIZE(8, 0x08),00,
+            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_RELATIVE),00,
+        HID_RI_END_COLLECTION(0),00,
+    HID_RI_END_COLLECTION(0),00,
 
 
-    HID_RI_USAGE_PAGE(8, 0x01),           // Generic Desktop
-    HID_RI_USAGE(8, 0x80),                // System Control
-    HID_RI_COLLECTION(8, 0x01),           // Application
-        HID_RI_REPORT_ID(8, 3),
-        HID_RI_USAGE_MINIMUM(8, 0x01),    // Pointer
-        HID_RI_USAGE_MAXIMUM(16, 0x00B7), // System Display LCD Autoscale
-        HID_RI_LOGICAL_MINIMUM(8, 0x01),
-        HID_RI_LOGICAL_MAXIMUM(16, 0x00B7),
-        HID_RI_REPORT_COUNT(8, 1),
-        HID_RI_REPORT_SIZE(8, 16),
-        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),
-    HID_RI_END_COLLECTION(0),
+    HID_RI_USAGE_PAGE(8, 0x01),00,           // Generic Desktop
+    HID_RI_USAGE(8, 0x80),00,                // System Control
+    HID_RI_COLLECTION(8, 0x01),00,           // Application
+        HID_RI_REPORT_ID(8, 3),00,
+        HID_RI_USAGE_MINIMUM(8, 0x01),00,    // Pointer
+        HID_RI_USAGE_MAXIMUM(16, 0x00B7),00, // System Display LCD Autoscale
+        HID_RI_LOGICAL_MINIMUM(8, 0x01),00,
+        HID_RI_LOGICAL_MAXIMUM(16, 0x00B7),00,
+        HID_RI_REPORT_COUNT(8, 1),00,
+        HID_RI_REPORT_SIZE(8, 16),00,
+        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),00,
+    HID_RI_END_COLLECTION(0),00,
 
-    HID_RI_USAGE_PAGE(8, 0x0C),           // Consumer
-    HID_RI_USAGE(8, 0x01),                // Consumer Control
-    HID_RI_COLLECTION(8, 0x01),           // Application
-        HID_RI_REPORT_ID(8, 4),
-        HID_RI_USAGE_MINIMUM(8, 0x01),    // Consumer Control
-        HID_RI_USAGE_MAXIMUM(16, 0x02A0), // AC Desktop Show All Applications
-        HID_RI_LOGICAL_MINIMUM(8, 0x01),
-        HID_RI_LOGICAL_MAXIMUM(16, 0x02A0),
-        HID_RI_REPORT_COUNT(8, 1),
-        HID_RI_REPORT_SIZE(8, 16),
-        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),
-    HID_RI_END_COLLECTION(0),
+    HID_RI_USAGE_PAGE(8, 0x0C),00,           // Consumer
+    HID_RI_USAGE(8, 0x01),00,                // Consumer Control
+    HID_RI_COLLECTION(8, 0x01),00,           // Application
+        HID_RI_REPORT_ID(8, 4),00,
+        HID_RI_USAGE_MINIMUM(8, 0x01),00,    // Consumer Control
+        HID_RI_USAGE_MAXIMUM(16, 0x02A0),00, // AC Desktop Show All Applications
+        HID_RI_LOGICAL_MINIMUM(8, 0x01),00,
+        HID_RI_LOGICAL_MAXIMUM(16, 0x02A0),00,
+        HID_RI_REPORT_COUNT(8, 1),00,
+        HID_RI_REPORT_SIZE(8, 16),00,
+        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),00,
+    HID_RI_END_COLLECTION(0),00,
 
 
-    HID_RI_USAGE_PAGE(8, 0x01),        // Generic Desktop
-    HID_RI_USAGE(8, 0x06),             // Keyboard
-    HID_RI_COLLECTION(8, 0x01),        // Application
-        HID_RI_REPORT_ID(8, 5),
+    HID_RI_USAGE_PAGE(8, 0x01),00,        // Generic Desktop
+    HID_RI_USAGE(8, 0x06),00,             // Keyboard
+    HID_RI_COLLECTION(8, 0x01),00,        // Application
+        HID_RI_REPORT_ID(8, 5),00,
         // Modifiers (8 bits)
-        HID_RI_USAGE_PAGE(8, 0x07),    // Keyboard/Keypad
-        HID_RI_USAGE_MINIMUM(8, 0xE0), // Keyboard Left Control
-        HID_RI_USAGE_MAXIMUM(8, 0xE7), // Keyboard Right GUI
-        HID_RI_LOGICAL_MINIMUM(8, 0x00),
-        HID_RI_LOGICAL_MAXIMUM(8, 0x01),
-        HID_RI_REPORT_COUNT(8, 0x08),
-        HID_RI_REPORT_SIZE(8, 0x01),
-        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+        HID_RI_USAGE_PAGE(8, 0x07),00,    // Keyboard/Keypad
+        HID_RI_USAGE_MINIMUM(8, 0xE0),00, // Keyboard Left Control
+        HID_RI_USAGE_MAXIMUM(8, 0xE7),00, // Keyboard Right GUI
+        HID_RI_LOGICAL_MINIMUM(8, 0x00),00,
+        HID_RI_LOGICAL_MAXIMUM(8, 0x01),00,
+        HID_RI_REPORT_COUNT(8, 0x08),00,
+        HID_RI_REPORT_SIZE(8, 0x01),00,
+        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),00,
         // Keycodes
-        HID_RI_USAGE_PAGE(8, 0x07),    // Keyboard/Keypad
-        HID_RI_USAGE_MINIMUM(8, 0x00),
-        HID_RI_USAGE_MAXIMUM(8, KEYBOARD_REPORT_BITS * 8 - 1),
-        HID_RI_LOGICAL_MINIMUM(8, 0x00),
-        HID_RI_LOGICAL_MAXIMUM(8, 0x01),
-        HID_RI_REPORT_COUNT(8, KEYBOARD_REPORT_BITS * 8),
-        HID_RI_REPORT_SIZE(8, 0x01),
-        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+        HID_RI_USAGE_PAGE(8, 0x07),00,    // Keyboard/Keypad
+        HID_RI_USAGE_MINIMUM(8, 0x00),00,
+        HID_RI_USAGE_MAXIMUM(8, KEYBOARD_REPORT_BITS * 8 - 1),00,
+        HID_RI_LOGICAL_MINIMUM(8, 0x00),00,
+        HID_RI_LOGICAL_MAXIMUM(8, 0x01),00,
+        HID_RI_REPORT_COUNT(8, KEYBOARD_REPORT_BITS * 8),00,
+        HID_RI_REPORT_SIZE(8, 0x01),00,
+        HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),00,
 
         // Status LEDs (5 bits)
-        HID_RI_USAGE_PAGE(8, 0x08),    // LED
-        HID_RI_USAGE_MINIMUM(8, 0x01), // Num Lock
-        HID_RI_USAGE_MAXIMUM(8, 0x05), // Kana
-        HID_RI_REPORT_COUNT(8, 0x05),
-        HID_RI_REPORT_SIZE(8, 0x01),
-        HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE | HID_IOF_NON_VOLATILE),
+        HID_RI_USAGE_PAGE(8, 0x08),00,    // LED
+        HID_RI_USAGE_MINIMUM(8, 0x01),00, // Num Lock
+        HID_RI_USAGE_MAXIMUM(8, 0x05),00, // Kana
+        HID_RI_REPORT_COUNT(8, 0x05),00,
+        HID_RI_REPORT_SIZE(8, 0x01),00,
+        HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE | HID_IOF_NON_VOLATILE),00,
         // LED padding (3 bits)
-        HID_RI_REPORT_COUNT(8, 0x01),
-        HID_RI_REPORT_SIZE(8, 0x03),
-        HID_RI_OUTPUT(8, HID_IOF_CONSTANT),
-    HID_RI_END_COLLECTION(0),
+        HID_RI_REPORT_COUNT(8, 0x01),00,
+        HID_RI_REPORT_SIZE(8, 0x03),00,
+        HID_RI_OUTPUT(8, HID_IOF_CONSTANT),00,
+    HID_RI_END_COLLECTION(0),00,
 };
 
 
